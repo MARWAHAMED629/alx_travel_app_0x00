8781de4a08c63e1a30d3f62f50c78e55c60b778d
MARWAHAMED629 <mmero385@gmail.com>
2025-06-15 22:44:51 +0200

    Initial commit: milestone 3 - API for Listings and Bookings

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -34,4 +34,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+  path('admin/', admin.site.urls),
+    path('api/', include('listings.urls')),
 ]
